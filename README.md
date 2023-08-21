176c801e7beaabf4408d5bb4884eea19909af1f6
zhoujifang <1836564213@qq.com>
2023-08-21 14:49:08 +0800

    feat: init

--- README.md
+++
@@ -1,2 +0,0 @@
-# VisualNoteHub
-Content management, and its implementation includes image management, document management, and blog content management
--- src/libs/api.request.js
+++
@@ -1,6 +0,0 @@
-import HttpRequest from '@/libs/axios'
-// import config from '@/config'
-const baseUrl = 'http://testapi.teacode.top:80'//process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
-
-const axios = new HttpRequest(baseUrl)
-export default axios
